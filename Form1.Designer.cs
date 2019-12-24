629f3f0b31b803ace838d876eae1dec9e17b9143
Keera Bray <keerabray@gmail.com>
2019-12-24 02:44:10 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace BEUATIFYULHUMANS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Buttonofdeath = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MustardCat = new System.Windows.Forms.PictureBox();
+            this.TmrApology = new System.Windows.Forms.Timer(this.components);
+            this.LblApology = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MustardCat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Buttonofdeath
+            // 
+            this.Buttonofdeath.BackColor = System.Drawing.Color.Cyan;
+            this.Buttonofdeath.Location = new System.Drawing.Point(105, 279);
+            this.Buttonofdeath.Name = "Buttonofdeath";
+            this.Buttonofdeath.Size = new System.Drawing.Size(125, 44);
+            this.Buttonofdeath.TabIndex = 0;
+            this.Buttonofdeath.Text = "Click Me!";
+            this.Buttonofdeath.UseVisualStyleBackColor = false;
+            this.Buttonofdeath.Click += new System.EventHandler(this.Buttonofdeath_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.BackColor = System.Drawing.Color.Thistle;
+            this.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Label1.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Black;
+            this.Label1.Location = new System.Drawing.Point(32, 128);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(276, 34);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "I heard you were having a bad day! ):";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Thistle;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(78, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hello friend!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Thistle;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(44, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 45);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "I have something that juuuust might make ya feel better!!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Thistle;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(149, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "(it\'s me keera)";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MustardCat
+            // 
+            this.MustardCat.Image = global::BEUATIFYULHUMANS.Properties.Resources.Mustard_Cat;
+            this.MustardCat.Location = new System.Drawing.Point(-1, 0);
+            this.MustardCat.Name = "MustardCat";
+            this.MustardCat.Size = new System.Drawing.Size(343, 364);
+            this.MustardCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MustardCat.TabIndex = 5;
+            this.MustardCat.TabStop = false;
+            // 
+            // TmrApology
+            // 
+            this.TmrApology.Interval = 2500;
+            this.TmrApology.Tick += new System.EventHandler(this.TmrApology_Tick);
+            // 
+            // LblApology
+            // 
+            this.LblApology.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblApology.Location = new System.Drawing.Point(99, 203);
+            this.LblApology.Name = "LblApology";
+            this.LblApology.Size = new System.Drawing.Size(209, 109);
+            this.LblApology.TabIndex = 6;
+            this.LblApology.Text = "Oh shit oh fuck im so sorry you had to see that";
+            this.LblApology.Click += new System.EventHandler(this.LblApology_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(340, 360);
+            this.Controls.Add(this.LblApology);
+            this.Controls.Add(this.MustardCat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Buttonofdeath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Hello!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MustardCat)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Buttonofdeath;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox MustardCat;
+        private System.Windows.Forms.Timer TmrApology;
+        private System.Windows.Forms.Label LblApology;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BEUATIFYULHUMANS
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MustardCat.Visible = false;
+            LblApology.Visible = false;
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Buttonofdeath_Click(object sender, EventArgs e)
+        {
+
+            MustardCat.Visible = true;
+            TmrApology.Start();
+        }
+        //int Timer = 0;
+        private void TmrApology_Tick(object sender, EventArgs e)
+        {
+
+            LblApology.Visible = true;
+
+
+            //look at this actual idiotic bullshit that i tried to code
+            //Timer++;
+
+            //if (Timer == 4)
+            //     LblApology.Visible = true;
+            //else if (Timer == 8)
+            //    Label1.Visible = true;
+            //else
+            //    TmrApology.Stop();
+            //this.Close();
+
+
+        }
+
+        private void LblApology_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Shutdown_Tick(object sender, EventArgs e)
+        {
+            Visible = false;
+
+        }
+    }
+}
